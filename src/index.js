f32aea2c380f8cad08e0958914d410b9911abe61
Avinash Biradar <74947684+avibiradar100@users.noreply.github.com>
2021-08-31 19:17:42 +0530

    updated index.js

--- src/index.js
+++ src/index.js
@@ -2,3 +2,19 @@
 //It should display a h1 heading.
 //It should display an unordered list (bullet points).
 //It should contain 3 list elements.
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Favourite cricketer</h1>
+    <ul>
+      <li>Rohit sharma</li>
+      <li>Suryakumar yadav</li>
+      <li>jasprit bumrah</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
+
